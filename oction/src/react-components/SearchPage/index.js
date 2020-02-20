680499dc76d3e4060d71e44d60a91993267ae2c3
WANG Shuoer <wangs415@teach.cs.toronto.edu>
2020-02-19 23:37:10 -0500

    try material ui

--- oction/src/react-components/SearchPage/index.js
+++ oction/src/react-components/SearchPage/index.js
@@ -3,17 +3,57 @@ import React from "react";
 import "./styles.css";
 
 import Header from '../Header';
+import SearchItem from '../SearchItem';
+import { Grid, Paper, Typography } from '@material-ui/core';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import { posts } from "./posts.js";
 
 /* The SearchPage Component */
 class SearchPage extends React.Component {
 
+  renderItems() {
+    return <SearchItem />;
+  }
+  
   render() {
-    const {searchInput} = this.props;
+    // const {searchInput} = this.props;
     
     return (
-      <div className="search__bg-image center">
-        <Header/>
-      </div>
+        <div className="search__bg-image center">
+          <Header/>
+          <body>
+            <div class="right">
+            <Grid container spacing={40} justify="center">
+        	    {posts.map(post => (
+          	    <Grid item key={post.name}>
+            	    <Card>
+                  <CardActionArea>
+                  <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="140"
+                  image={post.image}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {post.name}
+                  </Typography>
+                  <Typography component="p">{post.price}</Typography>
+                </CardContent>
+              </CardActionArea> 
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+            </div>
+          </body>
+        </div> 
     );
   }
 }
